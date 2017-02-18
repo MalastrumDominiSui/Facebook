adf9651ee7cd0113605cb6a5d67321d390ac0a9d
Michael Sefranek <msefran@gmail.com>
2017-02-18 15:16:42 -0600

    printed JSON to console

--- ajax.js
+++ ajax.js
@@ -1,3 +1,9 @@
+// access facebook data via JSON
 var request = new XMLHttpRequest();
-request.open("GET", "commentData.json");
-console.log(request.responseText);
+request.open("GET", "https://raw.githubusercontent.com/MalastrumDominiSui/Facebook/master/commentData.json");
+request.onload = function(){
+	var faceData = JSON.parse(request.responseText);
+};
+request.send();
+
+
